262d6a7faaab43734bf19b07ad566005346d4f58
merikonjatta <merikonjatta@gmail.com>
2011-02-25 08:33:07 +0900

    Tests

---
+++ test/unit/wiki_page_test.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class WikiPageTest < ActiveSupport::TestCase
+  fixtures :wiki_pages
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
+
